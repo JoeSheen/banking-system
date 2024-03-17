a479863179a078a1728fdb78e2cb098aa0fe3024
Joe Sheen <joe123sheen@hotmail.com>
2024-03-17 15:43:57 +0000

    Initial commit

---
+++ src/test/java/com/sheen/joe/bankingsystem/BankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheen.joe.bankingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
